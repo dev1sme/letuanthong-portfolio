33eb87b5a3146ee72dd09b7641acdb7d3b74bbf3
Le Tuan Thong <letuanthong350@gmail.com>
2026-02-04 16:57:50 +0700

    update base url

--- src/App.tsx
+++ src/App.tsx
@@ -17,7 +17,7 @@ const App = () => (
         <TooltipProvider>
           <Toaster />
           <Sonner />
-          <BrowserRouter basename="/letuanthong-portfolio">
+          <BrowserRouter basename="/portfolio">
             <Routes>
               <Route path="/" element={<Index />} />
               {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
